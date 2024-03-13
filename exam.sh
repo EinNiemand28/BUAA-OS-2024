c122f038de6564bb09d62054061b3d52a6db99ed
彭正宇 <21376317@buaa.edu.cn>
2024-03-13 19:39:10 +0800

    try

--- exam.sh
+++ exam.sh
@@ -5,3 +5,22 @@ mkdir test
 cp -r code ./test/code
 
 cat code/14.c
+
+i=0
+while [ $i -le 15 ]
+do
+	gcc -c test/code/$i.c
+	mv $i.o test/code/$i.o
+	i=$[$i+1]
+done
+
+j=0
+while [ $j -le 15 ]
+do
+	gcc -o test/code/$i.o hello
+	j=$[$j+1]
+done
+
+./test/hello 2> /test/err.txt
+
+mv test/err.txt err.txt
