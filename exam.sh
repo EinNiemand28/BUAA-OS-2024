6019f7b07737cff6760a464eac3ee0ccc5c9d7b4
彭正宇 <21376317@buaa.edu.cn>
2024-03-13 19:24:31 +0800

    try

--- exam.sh
+++ exam.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
 mkdir test
-
-cp -r ./code ./test/code
-
-cat ./code/14.c
-
-i=0
-while [ $i -le 15 ]
-do
-	gcc ./test/code/$i.c -c ./test/code/$i.o
-	let i=i+1
-done
-
-gcc -o ./test/code/*.o hello
