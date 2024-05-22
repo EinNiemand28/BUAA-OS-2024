e36b5800248b824b4e299223f7922dea69233d11
彭正宇 <21376317@buaa.edu.cn>
2024-05-22 20:03:25 +0800

    first try

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -17,8 +17,27 @@ void strace_send(int sysno) {
 	}
 
 	// Your code here. (1/2)
+	if (straced) {
+		int tmp = straced;
+		straced = 0;
+		int env_id = syscall_getenvid();
+		ipc_send(envs[ENVX(env_id)].env_parent_id, sysno, 0, 0);
+		syscall_set_env_status(env_id, ENV_NOT_RUNNABLE);
+		straced = tmp;
+	}
 }
 
 void strace_recv() {
 	// Your code here. (2/2)
+	int r;
+	u_int child_id;
+	while (1) {
+		r = ipc_recv(&child_id, 0, 0);
+		strace_barrier(child_id);
+		recv_sysno(child_id, r);
+		syscall_set_env_status(child_id, ENV_RUNNABLE);
+		if (r == SYS_env_destroy) {
+			break;
+		}
+	}
 }
